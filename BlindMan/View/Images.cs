62b1f30eae7855a5ffa1d4e7f7028eaa7e39a863
Gorr <gorrgorrob@gmail.com>
2020-05-07 11:49:22 +0500

    graphics update

--- BlindMan/View/Images.cs
+++ BlindMan/View/Images.cs
@@ -5,7 +5,6 @@ namespace BlindMan.View
     public class Images
     {
         public Image Player { get; private set; }
-        public Image Wall { get; private set; }
         public Image DoorClosed { get; private set; }
         public Image DoorOpen { get; private set; }
         public Image Key { get; private set; }
@@ -18,10 +17,9 @@ namespace BlindMan.View
         public void Load()
         {
             Player = LoadImageFromAssets("player.png");
-            Wall = LoadImageFromAssets("wall.png");
             DoorClosed = LoadImageFromAssets("door_closed.png");
             DoorOpen = LoadImageFromAssets("door_open.png");
-            Key = LoadImageFromAssets("key.jpg");
+            Key = LoadImageFromAssets("key.png");
             Glasses = LoadImageFromAssets("glasses.png");
             Portal = LoadImageFromAssets("portal.png");
         }
